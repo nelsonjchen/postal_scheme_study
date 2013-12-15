6bfcb48e38b3c0dc16162cf9ab70cc06b0d6b653
Nelson Chen <crazysim@gmail.com>
2013-12-15 15:52:43 -0800

    Take numbers in between

--- generate_anki.rb
+++ generate_anki.rb
@@ -7,10 +7,18 @@ File.open('anki_import.txt', 'w') do |file|
     # Delete own notation for odd or even
     addr = row[0]
     carrier = row[1]
-    clean_addr = addr.gsub(/ [A-Z]'/, '')
+
+    number_regex = /^(\d+)-(\d+)/
+    numbers = number_regex.match(addr)
+    example_addr_begin_number = numbers[1].to_i
+    example_addr_end_number = numbers[2].to_i
+    example_addr_number = (example_addr_begin_number + example_addr_end_number)/2
+    eg_addr = addr.gsub(number_regex, example_addr_number.to_s)
+
+    clean_addr = eg_addr.gsub(/ [A-Z]'/, '')
     full_addr = clean_addr + ', La Puente, CA ' + ARGV[1]
     uri_addr = URI.escape(full_addr)
-    filename = Digest::MD5.hexdigest(addr) + '.png'
-    file.puts full_addr + '<br/>' + '<img src=\'' + filename + '\'/>' + ';' + carrier
+    filename = Digest::MD5.hexdigest(eg_addr) + '.png'
+    file.puts full_addr + '<br/>' + '<img src=\'' + filename + '\'/><br/> eg. ' + eg_addr + ';' + carrier
   end
 end
--- generate_dl.rb
+++ generate_dl.rb
@@ -7,10 +7,18 @@ File.open('aria2dl.txt', 'w') do |file|
     url = 'http://maps.googleapis.com/maps/api/staticmap?size=600x300&sensor=false&key=AIzaSyBZh3BSaHzLRuVQTka_NpTHmvLqg5sWdrM&markers='
     # Delete own notation for odd or even
     addr = row[0]
-    clean_addr = addr.gsub(/ [A-Z]'/, '')
+
+    number_regex = /^(\d+)-(\d+)/
+    numbers = number_regex.match(addr)
+    example_addr_begin_number = numbers[1].to_i
+    example_addr_end_number = numbers[2].to_i
+    example_addr_number = (example_addr_begin_number + example_addr_end_number)/2
+    eg_addr = addr.gsub(number_regex, example_addr_number.to_s)
+
+    clean_addr = eg_addr.gsub(/ [A-Z]'/, '')
     full_addr = clean_addr + ', La Puente, CA ' + ARGV[1]
     uri_addr = URI.escape(full_addr)
-    filename = Digest::MD5.hexdigest(addr) + '.png'
+    filename = Digest::MD5.hexdigest(eg_addr) + '.png'
     file.puts url + uri_addr + "\n  " + 'out=' + filename + "\n  " + 'dir=downloads'
   end
 end
