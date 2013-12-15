5a2424b54d04ded3e7806ba7e21c027a6cc0c224
Nelson Chen <crazysim@gmail.com>
2013-12-15 15:18:52 -0800

    Append to a file

--- generate_dl.rb
+++ generate_dl.rb
@@ -2,14 +2,16 @@ require 'csv'
 require 'uri'
 require 'digest'
 
-CSV.foreach(ARGV[0]) do |row|
-  url = 'http://maps.googleapis.com/maps/api/staticmap?size=600x300&sensor=false&key
+File.open('aria2dl.txt', 'w') do |file|
+  CSV.foreach(ARGV[0]) do |row|
+    url = 'http://maps.googleapis.com/maps/api/staticmap?size=600x300&sensor=false&key
 =AIzaSyBZh3BSaHzLRuVQTka_NpTHmvLqg5sWdrM&markers='
-  # Delete own notation for odd or even
-  addr = row[0]
-  clean_addr = addr.gsub(/ [A-Z]'/, '')
-  full_addr = clean_addr + ', La Puente, CA ' + ARGV[1]
-  uri_addr = URI.escape(full_addr)
-  filename = Digest::MD5.hexdigest(addr)
-  puts url + uri_addr + "\n  " + 'out=' + filename + "\n  " + 'dir=downloads/'
+    # Delete own notation for odd or even
+    addr = row[0]
+    clean_addr = addr.gsub(/ [A-Z]'/, '')
+    full_addr = clean_addr + ', La Puente, CA ' + ARGV[1]
+    uri_addr = URI.escape(full_addr)
+    filename = Digest::MD5.hexdigest(addr)
+    file.puts url + uri_addr + "\n  " + 'out=' + filename + "\n  " + 'dir=downloads/'
+  end
 end
