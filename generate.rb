ec4b0091d8cf53c9d1a7b048f039ce1b700378ad
Nelson Chen <crazysim@gmail.com>
2013-12-15 15:06:41 -0800

    Take in some arguments

--- generate.rb
+++
@@ -1,6 +0,0 @@
-require 'csv'
-require 'cgi'
-
-CSV.foreach('./input.csv') do |row|
-  puts row[0], row[1]
-end
---
+++ generate_dl.rb
@@ -0,0 +1,11 @@
+require 'csv'
+require 'uri'
+
+CSV.foreach(ARGV[0]) do |row|
+  url = 'http://maps.googleapis.com/maps/api/staticmap?size=600x300&sensor=false&key
+=AIzaSyBZh3BSaHzLRuVQTka_NpTHmvLqg5sWdrM&markers='
+  # Delete own notation for odd or even
+  addr = row[0].gsub(/ [A-Z]'/,'')
+  full_addr = addr + ', La Puente, CA ' + ARGV[1]
+  puts full_addr
+end
