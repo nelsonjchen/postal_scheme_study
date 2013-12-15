b5197605e1cfa722bc1a4f721e69e34f047e55cf
Nelson Chen <crazysim@gmail.com>
2013-12-15 15:27:20 -0800

    Make quick ANKI study card generator

--- generate_anki.rb
+++ generate_anki.rb
@@ -2,15 +2,15 @@ require 'csv'
 require 'uri'
 require 'digest'
 
-File.open('aria2dl.txt', 'w') do |file|
+File.open('anki_import.txt', 'w') do |file|
   CSV.foreach(ARGV[0]) do |row|
-    url = 'http://maps.googleapis.com/maps/api/staticmap?size=600x300&sensor=false&key=AIzaSyBZh3BSaHzLRuVQTka_NpTHmvLqg5sWdrM&markers='
     # Delete own notation for odd or even
     addr = row[0]
+    carrier = row[1]
     clean_addr = addr.gsub(/ [A-Z]'/, '')
     full_addr = clean_addr + ', La Puente, CA ' + ARGV[1]
     uri_addr = URI.escape(full_addr)
     filename = Digest::MD5.hexdigest(addr) + '.png'
-    file.puts url + uri_addr + "\n  " + 'out=' + filename + "\n  " + 'dir=downloads'
+    file.puts full_addr + '<br/>' + '<img src=\'' + filename + '\'/>' + ';' + carrier
   end
 end
